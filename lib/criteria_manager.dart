57fbb2545b11335c1a44268cca500d4be934ab16
Liveeeez <42978134+Liveeeez@users.noreply.github.com>
2019-09-07 20:28:25 +1000

    fixing inconsistencies
    changed criteria_manager to same as post branch to avoid merge errors and added students.dart file

--- lib/criteria_manager.dart
+++ lib/criteria_manager.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-// import 'dart:async';
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
+//import 'dart:async';
+//import 'dart:convert';
+//import 'package:http/http.dart' as http;
 
 import './main.dart';
 
@@ -78,4 +78,3 @@ class CriteriaManager extends StatelessWidget {
     );
   }
 }
-
